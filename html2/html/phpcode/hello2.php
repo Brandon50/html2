f78a61726e19dc67d21c5f8d39d1f539fa0b4b55
Brandon50 <Brandonweston77@yahoo.com>
2016-10-26 08:40:42 -0700

    Add files via upload

---
+++ html2/html/phpcode/hello2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+     $color = "#ff7f00";
+?>
+<body bgcolor=<?php echo "$color"; ?>";
+$txt1 = "Kilo";
+$txt2 = "10.183.1.11";
+$txt3 = "Hellooooooooooo my fellow Americans";
+$x = 5;
+$y = 4;
+$z = 65521;
+
+echo "<h2>" . $txt1 . "</h2>";
+echo "My ip: " . $txt2 . "<br>";
+echo "$txt3";
+echo $x + $y;
+?>
+
+</body>
+</html>
